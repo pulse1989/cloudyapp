3f479d0fdf5cdf44bec9adc5db5904ac126806c4
vusimoyo <vusumuzi.moyo@absa.co.za>
2019-01-16 15:07:17 +0200

    removed spacing

--- app/src/main/java/za/co/kernelpanic/cloudy/ui/activities/WeatherActivity.java
+++ app/src/main/java/za/co/kernelpanic/cloudy/ui/activities/WeatherActivity.java
@@ -6,14 +6,18 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
 import com.google.android.material.snackbar.Snackbar;
+
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 import android.widget.Toast;
 
@@ -36,12 +40,13 @@ import dagger.android.support.HasSupportFragmentInjector;
 import za.co.kernelpanic.cloudy.R;
 import za.co.kernelpanic.cloudy.ui.fragments.Forecast.WeatherFragment;
 
-public class WeatherActivity extends AppCompatActivity  implements HasSupportFragmentInjector{
+public class WeatherActivity extends AppCompatActivity implements HasSupportFragmentInjector {
 
     @Inject
     DispatchingAndroidInjector<Fragment> androidInjector; //we need to inject our android fragment instances into the activity
     private static final String LOG_TAG = WeatherActivity.class.getSimpleName();
-    @Inject LocationRequest locationRequest;
+    @Inject
+    LocationRequest locationRequest;
 
     private static final int CHECK_PLAY_SERVICES = 9000;
     private static final int FINE_LOCATION_REQUEST_CODE = 8;
@@ -53,11 +58,11 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-         /*
-          * we need this here before calling onCreate otherwise our fragment attachments can end up failing
-          * as there is a possibility the fragment can get attached while the activity calles super().
-          * Before that happens, our activity has to be injected! otherwise any fragments that rely on this activity can crash
-          */
+        /*
+         * we need this here before calling onCreate otherwise our fragment attachments can end up failing
+         * as there is a possibility the fragment can get attached while the activity calles super().
+         * Before that happens, our activity has to be injected! otherwise any fragments that rely on this activity can crash
+         */
         AndroidInjection.inject(this);
 
         //We then continue as normal
@@ -71,12 +76,9 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
          * Only if we haven't got anything in onSaveInstanceState and we've confirmed checkplay services is here.
          * we can go ahead and attach the fragment
          */
-        if(savedInstanceState == null && checkPlayServices() ) {
-
+        if (savedInstanceState == null && checkPlayServices()) {
             requestPermissions();
-
-         }
-
+        }
     }
 
     @Override
@@ -85,7 +87,6 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
     }
 
     private void loadFragment() {
-
         WeatherFragment weatherFragment = new WeatherFragment();
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction().add(R.id.mainLayout, weatherFragment, "weather");
@@ -97,76 +98,55 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
      * As we are using Google Play Services new Location API for this app, we have to ensure all dependencies are met.
      * This includes using the correct version of play services that is on the device. We take care of that here.
      */
-    private boolean checkPlayServices(){
-
+    private boolean checkPlayServices() {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
         int result = apiAvailability.isGooglePlayServicesAvailable(this);
-        if(result != ConnectionResult.SUCCESS) {
-
-            if(apiAvailability.isUserResolvableError(result)) {
-                apiAvailability.getErrorDialog(this, result,  CHECK_PLAY_SERVICES).show();
+        if (result != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(result)) {
+                apiAvailability.getErrorDialog(this, result, CHECK_PLAY_SERVICES).show();
             }
-
             Log.e(LOG_TAG, "Play Services package is missing, unable to continue");
-
             return false;
-
         } else {
-
             Log.d(LOG_TAG, "We have play services installed. continuing...");
-
             return true;
         }
-
     }
-
 
     @Override
     protected void onResume() {
-
         super.onResume();
-
-        if(!checkPermissions()) {
-
+        if (!checkPermissions()) {
             requestPermissions();
-
         }
     }
 
     /*
-         * if this is the first time we're launching, let's ask the user for Location permissions before implementing this api. We need to do that here
-         * before we can subscribe to location updates. We'll include a decent rationale as well to let them know this is so we get the correct weather
-         * from their location
-         */
+     * if this is the first time we're launching, let's ask the user for Location permissions before implementing this api. We need to do that here
+     * before we can subscribe to location updates. We'll include a decent rationale as well to let them know this is so we get the correct weather
+     * from their location
+     */
     private boolean checkPermissions() {
-
         int permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-
         return permissionState == PackageManager.PERMISSION_GRANTED;
-
     }
 
 
     private void requestPermissions() {
-
         boolean shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION);
-
-        if(shouldProvideRationale) {
-
-            Log.i(LOG_TAG, "showing rational to the user for more information");
-
+        if (shouldProvideRationale) {
+            Log.i(LOG_TAG, "showing rational to the user for more information");=
             Snackbar.make(weatherActivityLayout, R.string.location_rationale,
                     Snackbar.LENGTH_INDEFINITE)
                     .setAction(R.string.ok,
-                    v -> ActivityCompat.requestPermissions(WeatherActivity.this, new String[]
-                           {Manifest.permission.ACCESS_FINE_LOCATION},
-                           FINE_LOCATION_REQUEST_CODE))
+                            v -> ActivityCompat.requestPermissions(WeatherActivity.this, new String[]
+                                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                                    FINE_LOCATION_REQUEST_CODE))
                     .show();
 
         } else {
-
             Log.i(LOG_TAG, "Requesting user permission");
-            ActivityCompat.requestPermissions(WeatherActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(WeatherActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
         }
     }
 
@@ -175,36 +155,26 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        switch(requestCode) {
-
+        switch (requestCode) {
             case FINE_LOCATION_REQUEST_CODE:
-
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d(LOG_TAG, "We have permissions! proceeding...");
                     loadFragment();
                     buildLocationSettingsRequest();
                     checkDeviceSettings();
-
-
                 } else {
-
                     Log.w(LOG_TAG, "We've been denied permissions, Therefor we cannot give accurate weather information");
-
                 }
                 break;
-
         }
     }
 
-    private void buildLocationSettingsRequest(){
+    private void buildLocationSettingsRequest() {
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
         builder.addLocationRequest(locationRequest);
         locationSettingsRequest = builder.build();
     }
-
 
     /*
      * here, we check if the device settings for the location are actually on. We may have permission, but the device needs
@@ -214,42 +184,29 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
      */
 
     private void checkDeviceSettings() {
-
         settingsClient.checkLocationSettings(locationSettingsRequest)
-
                 .addOnSuccessListener(this, locationSettingsResponse -> {
-
                     Log.i(LOG_TAG, "User has correct setting set.");
                 })
                 .addOnFailureListener(this, e -> {
-
-                    int statusCode = ((ApiException)e).getStatusCode();
-
+                    int statusCode = ((ApiException) e).getStatusCode();
                     switch (statusCode) {
-
                         case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-
                             Log.w(LOG_TAG, "We don't have the required settings. trying to resolve..");
-
                             try {
-                                     ResolvableApiException rae = (ResolvableApiException)e;
-                                     rae.startResolutionForResult(WeatherActivity.this, CHECK_DEVICE_SETTINGS);
+                                ResolvableApiException rae = (ResolvableApiException) e;
+                                rae.startResolutionForResult(WeatherActivity.this, CHECK_DEVICE_SETTINGS);
 
                             } catch (IntentSender.SendIntentException ise) {
 
                                 Log.w(LOG_TAG, "Unable to execute PendingIntent");
                             }
-
                             break;
-
                         case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-
                             String errorMessage = "Unable to solve settings via this App. Please resolve in location settings";
                             Log.w(LOG_TAG, errorMessage);
                             Toast.makeText(WeatherActivity.this, errorMessage, Toast.LENGTH_LONG).show();
-
                             break;
-
                     }
                 });
 
@@ -263,23 +220,16 @@ public class WeatherActivity extends AppCompatActivity  implements HasSupportFra
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode){
-
+        switch (requestCode) {
             case CHECK_DEVICE_SETTINGS:
-
-                switch (resultCode){
-
+                switch (resultCode) {
                     case Activity.RESULT_OK:
                         Log.i(LOG_TAG, "The user has agreed to make the settings change");
-                         break;
-
-                         case Activity.RESULT_CANCELED:
-                         Log.w(LOG_TAG, "The user cancelled request, they'll have to fix it manually");
-
-                         break;
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.w(LOG_TAG, "The user cancelled request, they'll have to fix it manually");
+                        break;
                 }
-
                 break;
 
         }
